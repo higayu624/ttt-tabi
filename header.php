0e5b371580b71066358204633ba3244d265248e7
higayu624 <higayu624@gmail.com>
2022-11-15 16:20:20 +0900

    tailwind cdn

---
+++ header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title></title>
+    <meta name="description">
+</head>
+<body class="text-base m-0 tracking-widest">
+    <header>
+        <div class="sticky w-full h-20 flex max-w-5xl mx-auto justify-between">
+            <div class="items-center leading-10 px-2.5 py-5 overflow-hidden">
+                <a href="/">ttt-tabi</a>
+            </div>
+            <ul class="flex items-center leading-10 px-2.5 py-3 overflow-hidden">
+                <li><a href="/">HOME</a></li>
+                <li><a href="/works">WORKS</a></li>
+                <li><a href="/about">ABOUT</a></li>
+                <li><a href="/skill">SKILL</a></li>
+                <li><a href="#service">SERVICE</a></li>
+                <li><a href="/contact">CONTACT</a></li>
+            </ul>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<div class="total-back"></div>
+<div class="top">
+    <div class="top-main">
+        <h2 class="top-title">Hiyokko Engineer</h2>
+        <p class="top-subtitle">Yuma.H</p>
+    </div>
+</div>
+<main class="main">
+    <section class="top-bottom">
+        <div>こんにちは</div>
+    </section>
+</main>
+<?php get_footer(); ?>
