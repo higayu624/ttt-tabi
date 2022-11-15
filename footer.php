4640c30fd619046a1bc4cc552268e793fa7ce381
Kametani <e20062@e.hiroshima-cu.ac.jp>
2022-11-15 17:09:42 +0900

    feature footer

---
+++ footer.php
@@ -0,0 +1,21 @@
+    <footer>
+        <div class = "flex justify-around">
+            <ul>
+                <li>HOME</li>
+                <li>TABI</li>
+                <li>サイズの選び方</li>
+                <li>お問い合わせ</li>
+            </ul>
+            <ul class =" ">
+
+                <li>特定商取引法人</li>
+                <li>プライバシー</li>
+                
+
+            </ul>
+            
+
+        </div>
+    </footer>
+
+</body>
