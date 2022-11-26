afcc5c048701cdbcc88b232ca526b345f0af5738
kametani-m <e20062@e.hiroshima-cu.ac.jp>
2022-11-26 20:58:44 +0900

    feature size

---
+++ page-size.php
@@ -0,0 +1,116 @@
+<?php get_header(); ?>
+
+<div class="max-w-5xl pb-10" style="margin:0 auto;">
+    <h1 class="object-center text-4xl m-5">足のサイズの測り方</h1><br>
+    
+    <div class="w-full">
+        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/size/1.jpg">
+    </div>
+
+    <div class="text-center my-10">
+        <div class="text-3xl p-5">自分に合った靴を選ぶ為に、<br>
+            足のサイズを測ってみましょう<br><br>
+        </div>
+        足のサイズを知るためには<br>
+
+        <span class = "font-bold bg-gray-200 text-lg">　足長（そくちょう）</span>　<span class = "font-bold bg-gray-200">　足囲（そくい）</span>を計測します。<br><br>
+
+        ご自宅で出来る計測方法をご紹介しますので、正しい足のサイズを測ってみて下さい。<br><br>
+    </div>
+
+    <p class = "font-bold bg-gray-200 text-lg">　用意するもの<br></p>
+
+    
+    紙（プリントできる方は足長計測シート）、鉛筆、三角定規、定規（30㎝程度）、メジャー<br><br>
+
+    <span class="text-blue-500 my-5"><a href="https://marugo-online.jp/ashikeisoku-seat.pdf">足長計測シートはこちら</a></span><br>
+
+    ※用紙サイズA４を指定し印刷してください。2枚印刷されますので、合わせ位置を重ね貼り合わせてご利用ください。<br>
+    ※印刷の際は、『実際のサイズ』をお選びください。『用紙に合わせる』をご選択された場合、正しい寸法で印刷されません。<br>
+    印刷後、正しく印刷されているか定規等で確認してからご利用ください。<br><br>
+
+    <p class = "font-bold bg-gray-200 text-lg">　測るときのポイント</p><br>
+
+    <ul>
+        ・両足を肩幅の広さに開いて直立します。<br>
+        ・両足に均等に体重を乗せます<br>
+        ・目線はまっすぐ前を見ます<br>
+        ・両足を測って、左右のサイズが違う場合は大きい方のサイズに合わせましょう。<br><br>
+
+        ★一人では正しい姿勢が保てないので、計測の際は他の方にお願いすることをお勧めします。
+    </ul>
+
+    <div class="w-full">
+        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/size/2.jpg">
+    </div>
+    <p class = "font-bold bg-gray-200 text-lg">　足長の測り方</p><br>
+
+    <div>
+        【１】図１のように紙に直角に交わる線を引きます。<br>
+        ※計測シートを印刷されている方は【２】から。<br><br>
+    </div>
+    <div>
+        【２】かかとの一番出ている部分をAに合わせて、紙の上にまっすぐ立ちます。<br>
+        その時、人差し指の中心が縦の線と重なるようにします。<br>
+    </div>
+
+    <div class="w-full">
+        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/size/3.jpg">
+    </div>
+
+
+
+    【３】三角定規を図のように合わせ、一番長い指から真下に下した点を通るように線を引きます。（一番長い指から垂直に下した点を通り、AB線に直角に交わる線）<br><br>
+
+    A～Bの長さが『足長（そくちょう）』になります。<br>
+    ※どの指が一番長いかは人によって違います。<br><br>
+
+    <p class = "font-bold bg-gray-200 text-lg">　足囲の測り方</p><br>
+
+
+    <div class="w-full">
+        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/size/4.jpg">
+    </div>
+
+
+    親指の付け根と小指の付け根の一番張り出した骨の上を通って、ぐるっと一周メジャーで測ります。<br>
+    ※測定シートの端にある「足囲測定用メジャー」を切り取ってご活用下さい。<br><br>
+
+
+    <p class = "font-bold bg-gray-200 text-lg">　JIS表からサイズを確認</p><br>
+
+
+
+    【例】<br>
+    計測されたのが男性で、以下のような結果になった場合　<br>
+    ■右足　足長：252㎜　足囲：245㎜<br>
+    ■左足　足長：256㎜　足囲：250㎜<br>
+
+
+    <div class="w-full m-5">
+        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/size/5.png">
+    </div>
+
+
+
+    ■右足：25.0㎝E　■左足：25.5㎝EE　が足のサイズとなります。<br>
+    左右でサイズが違う場合、大きい方に合わせて靴をお選びください。<br>
+    今回の場合は<span class="bg-yellow-400">25.5㎝EEが靴の推奨サイズ</span>となります。<br>
+    ※メーカーや靴のデザインによって、大きさ・サイズ感は変わります。計測サイズは目安としてお考え下さい。<br><br>
+
+    <span class="font-bold">男性サイズ表</span><br>
+
+    <div class="w-full mb-10">
+        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/size/6.png">
+    </div>
+
+    <span class="font-bold">女性サイズ表</span><br>
+
+
+
+    <div class="w-full">
+        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/size/7.png">
+    </div>
+
+</div>
+<?php get_footer(); ?>
