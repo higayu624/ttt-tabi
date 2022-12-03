570d4469e95c0fec4a575f7c6e8be7aeb79d8ed2
kametani-m <e20062@e.hiroshima-cu.ac.jp>
2022-12-03 20:27:04 +0900

    fix/tabi

--- page-tabi.php
+++ page-tabi.php
@@ -1,11 +1,16 @@
 <?php get_header();?>
 <main>
-    <section class="py-16">
-        <p class="text-4xl font-serif text-center font-black ">TABIについて</p>
-    </section>
-    <div class="max-w-5xl" style="margin:0 auto;">
 
-        <div class="w-full text-center">
+    <div class="max-w-7xl" style="margin:0 auto;">
+        <p class="pt-5 text-4xl font-serif font-black mb-5">TABIについて</p>
+        <div class="py-5 flex justify-center">
+            <img class="w-1/2" src="<?php echo get_template_directory_uri(); ?>/images/tabi/6.jpg">
+        </div>
+    </div>
+
+    <div class="max-w-7xl justify-around" style="margin:0 auto;">
+
+        <div class="w-full pb-5">
             <p id="function" class="pb-5 pt-8 font-black text-lg">TABIの機能</p>
             <div>
                 足袋型の最大の特徴は親指と４趾に分かれていることです。<br>
@@ -15,67 +20,137 @@
                 アーチにはバネ作用・クッション性・バランス作用があり、鍛えることで3つの効果が期待できます。<br>
             </div>
         </div>
+    
+        <section class="w-full pb-5">
+            <div class="justify-center ">
+                <div class="inline-block w-70 px-5 h-full">
+                    <div class="w-70 overflow-visible">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/tabi/7.jpg">
+                    </div>
+                    <span class="text-center">体幹・バランス能力の向上<br>動的バランスと静的バランスの向上</span>
+                </div>
 
-        <div>
-            <section class="py-16">
-                <p class="text-2xl font-serif text-center font-black ">1．	体幹・バランス能力の向上　動的バランスと静的バランスの向上</p>
-            </section>
+                <div class="inline-block w-70 px-5 h-full">
+                    <div class="w-full overflow-visible">
+                        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/8.jpg">
+                    </div>
+                    <span class="text-center">スピード能力の向上</span>
+                </div>
 
-            <section class="py-16">
-                <p class="text-2xl font-serif text-center font-black ">2．	スピード能力の向上</p>
-            </section>
+                <div class="inline-block w-70 px-5 h-full">
+                    <div class="w-full overflow-visible ">
+                        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/9.jpg">
+                    </div>
+                    <span class="text-center">怪我防止 足指筋力の向上</span>
+                </div>
 
-            <div class="w-full  text-center">
-                <section class="py-16">
-                    <p class="text-2xl font-serif font-black ">3．	怪我防止 足指筋力の向上</p>
-                </section>
-                <div>
-                    足袋型シューズに足を通すと自然な形に指が開き、足裏本来のアーチが活かされ、パフォーマンスの向上につながります。
+            </div>
+        </section>
+
+        <section class="py-5">
+            <p class="text-2xl font-serif font-black ">1．	体幹・バランス能力の向上　動的バランスと静的バランスの向上</p>
+            <div class="flex pt-8">   
+                <div class ="max-w-2xl ">
+                丸五による実験では、足袋型スポーツシューズを5週間履き続けることで、足指筋力が向上するという結果になりました。<br>
+                足裏を鍛えることは下半身の筋力が高まるため、疲労を軽減し、怪我の防止にも繋がります。<br>
+                </div>
+                <div class="mx-3 flex">
+                    <div class="w-60 inline-block">
+                        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/10.jpg">
+                    </div>
+                    <div class="w-60 inline-block">
+                        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/11.jpg">
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
 
-        <div class="w-full text-center">
-            <p id="process" class="pb-5 pt-8 font-black text-lg">TABIの作り方</p>
-            <div class="pb-3">
-                丸五製の足袋靴は、足袋本来の木型（ラスト）を用いて作られています。<br>
-                そのため、足指に快適な空間を確保し、人間本来の足の機能を最大に活かした作り方をしています。<br>
-                またアッパー素材には伸縮性素材を使用しているため、長時間の着用でもムレにくく足の形にフィットさせた作り方となっています。<br>
-            </div>
-
-            <section class="w-full pb-5">
-                <div style="margin:0 auto; max-width:800px;">
-                    <div class="inline-block w-80">
-                        <div class="w-full">
-                            <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/4.jpg">
-                        </div>
-                    </div>
-                    <div class="inline-block w-80">
-                        <div class="w-full">
-                            <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/5.jpg">
-                        </div>
+        <section class="py-5">
+            <p class=" text-2xl font-serif font-black ">2．	スピード能力の向上</p>
+            <div class="flex pt-8">   
+                <div class ="max-w-2xl">
+                丸五による実験では足袋型スポーツシューズを5週間履き続けることで、歩行時の左右のブレ(左右方向への加速度)が少なくなり、
+                推進力(前方向への加速度)が増すという結果になりました。<br>
+                </div>
+                <div class="mx-3 flex">
+                    <div class="w-90">
+                        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/1.jpg">
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
+
+        <section class="py-5">
+            <p class="text-2xl font-serif font-black ">3．	怪我防止 足指筋力の向上</p>
+            <div class="flex pt-8">   
+                <div class ="max-w-2xl">
+                丸五による実験では、足袋型スポーツシューズを5週間履き続けることで、足指筋力が向上するという結果になりました。<br>
+                足裏を鍛えることは下半身の筋力が高まるため、疲労を軽減し、怪我の防止にも繋がります。<br>
+                </div>
+                <div class="mx-3 flex">
+                    <div class="w-90">
+                        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/12.jpg">
+                    </div>
+                </div>
+            </div>
+            <div class="pt-8 text-center text-2xl">
+                足袋型シューズに足を通すと自然な形に指が開き、足裏本来のアーチが活かされ、パフォーマンスの向上につながります。
+            </div>
+        </section>
+    
+
+
+        <p id="process" class="py-5 font-black text-lg">TABIの作り方</p>
+        <div class="pb-3">
+            丸五製の足袋靴は、足袋本来の木型（ラスト）を用いて作られています。<br>
+            そのため、足指に快適な空間を確保し、人間本来の足の機能を最大に活かした作り方をしています。<br>
+            またアッパー素材には伸縮性素材を使用しているため、長時間の着用でもムレにくく足の形にフィットさせた作り方となっています。<br>
         </div>
 
-        <div class="w-full text-center">
-            <p id="feature" class="pb-5 pt-8 font-black text-lg">TABIの特徴</p>
+        <section class="w-full pb-5">
+            <div style="margin:0 auto; max-width:800px;">
+                <div class="inline-block w-80">
+                    <div class="w-full">
+                        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/4.jpg">
+                    </div>
+                </div>
+                <div class="inline-block w-80">
+                    <div class="w-full">
+                        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/5.jpg">
+                    </div>
+                </div>
+            </div>
+        </section>
+    
+
+        <div class="w-full">
+            <p id="feature" class="py-5 font-black text-lg">TABIの特徴</p>
             <div class="py-5">
                 <span class="text-red-600">運動パフォーマンスを最大限に引き出す、TABI型！</span><br>
                 足の指はそれぞれに全く違う動きをする機能をもっています。<br>
                 一般的なシューズではその動きに制限がかかり、足指・足本来の機能が妨げられてしまいます。<br>
                 足袋型にすることで親指と人差し指の間に空間ができ、自由な指の動きが可能になります。<br>
                 自由に足指が使えることで普段使わない足指の筋肉が鍛えられ、外反母趾などの怪我のリスクも軽減されます。<br>
-                <span class="text-red-600">TABI型はあなたの足の潜在力を最大限に引き出します。</span><br>
             </div>
-
-            <div class="w-full pb-5">
-                <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/2.jpg">
-            </div>
+            <section class="w-full pb-5">
+                <div style="margin:0 auto; max-width:800px;">
+                    <div class="inline-block w-80">
+                        <div class="w-full">
+                            <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/2.jpg">
+                        </div>
+                    </div>
+                    <div class="inline-block w-80">
+                        <div class="w-full">
+                            <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/images/tabi/3.jpg">
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
-
-
     </div>
+    <div class="py-5 text-red-600 text-center text-3xl">
+        TABI型はあなたの足の潜在力を最大限に引き出します。
+    </div>
+
 </main>
 <?php get_footer();?>
